4f0187808bf5a56ae3908d7f821126b7c5f92ac1
이종문 <sdlwhdans98@gmail.com>
2025-05-09 09:30:13 +0900

    Product 컴포넌트

--- fe/src/shared/ui/Product/ProductItem.tsx
+++ fe/src/shared/ui/Product/ProductItem.tsx
@@ -1,3 +1,6 @@
+import { useNavigate } from "react-router-dom";
+import { Icon } from "@/shared/ui/Icon";
+
 type ProductItemProps = {
   postId: number;
   title: string;
@@ -9,28 +12,41 @@ type ProductItemProps = {
 };
 
 export const ProductItem = ({ product }: ProductItemProps) => {
+  const navigate = useNavigate();
   return (
-    <div className="w-full h-[136px] border">
-      <div className="flex w-full h-full gap-4">
+    <div className="w-full h-[136px] flex" onClick={()=>{navigate(`/product/${product.postId}`)}}>
+      <div className="h-full aspect-square">
         <img
           src={product.thumbnail}
-          alt={product.title}
-          className="h-full aspect-square rounded-xl object-cover"
+          className="w-full h-full object-cover rounded-xl"
         />
-
-        {/* 텍스트 정보 */}
-        <div className="flex flex-col justify-between py-1 flex-1">
-          <div className="text-sm text-gray-900 font-medium">
-            {product.title}
-          </div>
-          <div className="text-base font-bold">
-            {product.price.toLocaleString()}원
-          </div>
-          <div className="text-xs text-gray-500">
-            조회수 {product.view} · 좋아요 {product.like}
-          </div>
+      </div>
+      <div className="w-full h-full flex flex-col bg-gray-100">
+        <div>{product.title}</div>
+        {/* 가격은 나중에 , 넣는거 처리 하겠음 */}
+        <div>{product.price} 원</div>
+        <div className="flex gap-1">
+          <div>{product.like}</div>
+          <Icon
+            name={product.status ?  "heart" : "heartfill"}
+            className="shadow-xl w-6 h-6 rounded-full border"
+          />
         </div>
       </div>
     </div>
   );
 };
+
+// export const ProductItem = ({ product }: ProductItemProps) => {
+//   return (
+//     <div className="w-full h-[136px] flex bg-gray-200">
+//       <div className="h-full aspect-square bg-red-300">
+//         <img src={product.thumbnail} className="w-full h-full object-cover" />
+//       </div>
+//       <div className="w-full h-full flex flex-col">
+//         <div></div>
+//         <div></div>
+//       </div>
+//     </div>
+//   );
+// };
--- fe/src/shared/ui/Product/ProductList.tsx
+++ fe/src/shared/ui/Product/ProductList.tsx
@@ -1,8 +1,8 @@
 import { ProductItem, ProductItemProps } from "./ProductItem";
 
-export const ProductList = (products: ProductItemProps[]) => {
+export const ProductList = ({products}: ProductItemProps[]) => {
 	return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-2">
       {products.map((item) => (
         <ProductItem key={item.postId} product={item} />
       ))}
