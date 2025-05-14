b4a29cb4b2e149cc47ac1aaf34bfe1fb942cbb93
이종문 <sdlwhdans98@gmail.com>
2025-05-14 11:56:59 +0900

    temp : 로그인 test

--- fe/src/features/auth/ui/GoogleLoginButton2.tsx
+++ fe/src/features/auth/ui/GoogleLoginButton2.tsx
@@ -1,16 +1,16 @@
 import { FcGoogle } from "react-icons/fc";
-import { useGoogleLogin } from "../model/useGoogleLogin";
+import { useGoogleLogin2 } from "../model/useGoogleLogin2";
 
-export const GoogleLoginButton = () => {
-  const handleLogin = useGoogleLogin();
-  return (
+export const GoogleLoginButton2 = () => {
+	const handleLogin = useGoogleLogin2();
+	return (
     <button
       onClick={() => handleLogin()}
       className="w-[88%] h-[48px] bg-white/90 border-2 border-[#C6C6C6] rounded-xl flex justify-center items-center cursor-pointer"
     >
       <FcGoogle className="w-7 h-7" />
       <div className="mx-2 text-xl font-medium">
-        /oauth2/authorization/google
+        href oauth2/authorization/google
       </div>
     </button>
   );
--- fe/src/pages/login/LoginPage.tsx
+++ fe/src/pages/login/LoginPage.tsx
@@ -1,4 +1,7 @@
 import { GoogleLoginButton } from "@/features/auth/ui/GoogleLoginButton";
+import { GoogleLoginButton2 } from "@/features/auth/ui/GoogleLoginButton2";
+import { GoogleLoginButton3 } from "@/features/auth/ui/GoogleLoginButton3";
+import { GoogleLoginButton4 } from "@/features/auth/ui/GoogleLoginButton4";
 
 const LoginPage = () => {
   return (
@@ -18,16 +21,16 @@ const LoginPage = () => {
           pb-36"
       >
         <div className="flex flex-col justify-center items-center">
-          <img
-            src="/beet3.png"
-            className="w-[140px] mb-4 animate-bounce"
-          />
+          <img src="/beet3.png" className="w-[140px] mb-4 animate-bounce" />
           <h1 className="text-[#370035] text-4xl font-extrabold font-moneygraphy tracking-wide">
             비트마켓
           </h1>
         </div>
         <GoogleLoginButton />
-
+        <GoogleLoginButton3 />
+        <div className="mt-10"></div>
+        <GoogleLoginButton2 />
+        <GoogleLoginButton4 />
       </div>
     </div>
   );
