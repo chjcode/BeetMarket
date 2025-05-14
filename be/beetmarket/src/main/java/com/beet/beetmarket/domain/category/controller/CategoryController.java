40055d28c43a398ededa0d75da18037fc9c13980
조현석 <hsjo0227@gmail.com>
2025-05-14 09:39:49 +0900

    feat : category 조회 api 응답 형식 올바르게 수정

--- be/beetmarket/src/main/java/com/beet/beetmarket/domain/category/controller/CategoryController.java
+++ be/beetmarket/src/main/java/com/beet/beetmarket/domain/category/controller/CategoryController.java
@@ -1,7 +1,11 @@
 package com.beet.beetmarket.domain.category.controller;
 
 import com.beet.beetmarket.domain.category.service.CategoryService;
+import com.beet.beetmarket.global.response.ResponseWrapper;
+import com.beet.beetmarket.global.response.ResponseWrapperFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +23,12 @@ public class CategoryController {
     }
 
     @GetMapping
-    public List<String> getCategories() {
+    public ResponseEntity<ResponseWrapper<List<String>>> getCategories() {
 
-        return categoryService.findAll();
+        return ResponseWrapperFactory.setResponse(
+                HttpStatus.OK,
+                null,
+                categoryService.findAll()
+        );
     }
 }
