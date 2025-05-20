a3913d71e5b215f029454b95644db2fedb8bdbe7
이종문 <sdlwhdans98@gmail.com>
2025-05-20 10:05:56 +0900

    fix : 채팅 수정

--- fe/src/pages/chats/ChatRoomPage.tsx
+++ fe/src/pages/chats/ChatRoomPage.tsx
@@ -79,7 +79,8 @@ const ChatRoomPage: React.FC = () => {
   useEffect(() => {
     if (!roomId || !accessToken) return;
 
-    const socketUrl = `https://k12a307.p.ssafy.io/ws-chat?access-token=${accessToken}`;
+    // const socketUrl = `https://k12a307.p.ssafy.io/ws-chat?access-token=${accessToken}`;
+    const socketUrl = `https://beet.joonprac.shop:8700?access-token=${accessToken}`;
     const socket = new SockJS(socketUrl);
 
     const client = new Client({
