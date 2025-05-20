eed5ee86a7f361e14347ee5a9d896d89ac593638
최휘준 <chjcoder@gmail.com>
2025-05-20 12:58:10 +0900

    feat : 웹소켓 연결부 log 추가

--- chat/chatserver/chatserver/src/main/java/com/beet/chatserver/config/StompPrincipalHandshakeHandler.java
+++ chat/chatserver/chatserver/src/main/java/com/beet/chatserver/config/StompPrincipalHandshakeHandler.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class StompPrincipalHandshakeHandler extends DefaultHandshakeHandler {
     @Override
@@ -18,6 +21,17 @@ public class StompPrincipalHandshakeHandler extends DefaultHandshakeHandler {
     ) {
         String nickname = (String) attrs.get("nickname");
 
-        return () -> nickname;
+        //###
+        log.debug("Attempting to determine user. Handshake attributes: {}", attrs); // logger -> log
+        if (nickname != null && !nickname.isBlank()) {
+            log.info("Successfully determined user for WebSocket session. Nickname: '{}'", nickname); // logger -> log
+            return () -> nickname;
+        } else {
+            log.warn("Could not determine user from handshake attributes (nickname missing or empty). Request URI: {}", req.getURI()); // logger -> log
+            return super.determineUser(req, handler, attrs);
+        }
+        //####
+
+//        return () -> nickname;
     }
 }
