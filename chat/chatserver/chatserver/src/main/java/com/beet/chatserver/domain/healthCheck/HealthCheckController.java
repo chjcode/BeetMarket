57c986fb0b81f3acb9f1943c43b03737d72039a5
최휘준 <chjcoder@gmail.com>
2025-05-20 15:30:19 +0900

    feat : health check 로그 추가

--- chat/chatserver/chatserver/src/main/java/com/beet/chatserver/domain/healthCheck/HealthCheckController.java
+++ chat/chatserver/chatserver/src/main/java/com/beet/chatserver/domain/healthCheck/HealthCheckController.java
@@ -7,12 +7,16 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HealthCheckController {
 
     @GetMapping("/api/chat-health") // 테스트용 엔드포인트 경로
     public ResponseEntity<Map<String, Object>> checkChatServerHealth() {
         Map<String, Object> response = new HashMap<>();
+        log.info("채팅서버 health check");
         response.put("status", "Chat server is up and running!");
         response.put("serverTime", LocalDateTime.now().toString());
         return ResponseEntity.ok(response);
